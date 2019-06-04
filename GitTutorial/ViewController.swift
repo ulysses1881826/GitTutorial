2ea8f723e9374a61b09eb5e826c7e40252fd0554
Yuxiang Yang <ywj159676@gmail.com>
2019-06-04 21:53:13 +0800

    added button and print

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by lehu on 2019/6/4.
+//  Copyright © 2019 Hong Kong University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world!")
+    }
+
+
+}
+
